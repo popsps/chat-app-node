06d9b7eb9dd7e2001e58735dceb073b5e606139a
popsps <59066663+popsps@users.noreply.github.com>
2020-07-02 09:53:45 -0400

    build folder added

---
+++ frontend/build/precache-manifest.4941504f921b198a4e49d2ea5378679c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcffc985bf281ab44adc732e1ff28cc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b3e59d9d679c59110c8",
+    "url": "/static/css/main.5ded2852.chunk.css"
+  },
+  {
+    "revision": "ff6e045a101d0c5e0a35",
+    "url": "/static/js/2.fcf6d66f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fcf6d66f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3e59d9d679c59110c8",
+    "url": "/static/js/main.01a48d0b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
